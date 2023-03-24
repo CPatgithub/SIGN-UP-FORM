36d8fa6efa75d15e1fceff11a082a5f95e90c0f4
C P Singh <120619302+CPatgithub@users.noreply.github.com>
2023-03-25 01:34:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Form from "./Form";
+import { signUpFormValidation } from "../utils/validation";
+import WlecomeMsg from "./WlecomeMsg";
 
 const App = () => {
+  const [error,setError] = useState('')
+  const [email,setemail] = useState('')
+  function SubmitForm(formdata){
+    const ErrorMsg = signUpFormValidation(formdata)
+    if(ErrorMsg !== null){
+      setError(ErrorMsg)
+      setemail('');
+    }
+    else{
+      setError('')
+      setemail(formdata.email);
+    }
+  }
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Form SubmitForm={SubmitForm}/>
+      {email?<WlecomeMsg email={email}/> :error? <div className="error">*{error}</div>:null}
+      
+    </div>
   )
 }
 
